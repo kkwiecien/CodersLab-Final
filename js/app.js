4ce19f581c414e2f9af461f3a3ad44b44614a446
kkwiecien <kkwiecien00@gmail.com>
2016-07-27 12:17:42 +0200

    complete sliders and gallery display, add language button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function() {
 
   function navScroll() {
-    var $navButton = $('#menuBar a');
+    var $navButton = $('.navButtons a');
     $navButton.click(function(event){
       event.preventDefault();
       var $this = $(this);
@@ -12,6 +12,18 @@ $(function() {
   }
 
   function galleryDisplay() {
+    var $galleryDisplayButton = $('#artGallery').find('.three.columns').find('.galleryButton');
+    var $galleryContent = $('#artGallery').find('.nine.columns').find('p, section');
+    $galleryDisplayButton.on('click', function(event){
+      event.preventDefault();
+      var $this = $(this);
+      var show_what=$this.attr('data-type');
+      $('#artGallery').find('.nine.columns').find('.active').fadeOut("slow", function(){
+        $('#artGallery').find('.nine.columns').find('.active').removeClass('active');
+        $galleryContent.parent().find('section.'+show_what).slideDown(1000);
+        $galleryContent.parent().find('section.'+show_what).addClass('active');
+      });
+    });
   }
 
   function galleryScroll() {
@@ -51,48 +63,22 @@ $(function() {
   }
 
   function hobbyDisplay() {
-  }
-
-  function hobbyScroll() {
-    var $hobbyLeftButton = $('#hobbyGallery .sliderButtonLeft');
-    var $hobbyRightButton = $('#hobbyGallery .sliderButtonRight');
-    var index = 0;
-    $hobbyLeftButton.click(function(event){
-      var sliderLength = $(this).parent().find('div.imageFrame').find('ul').find('li').length;
-      index++;
-      if (index > sliderLength-1) {
-        index = 0;
-        $(this).parent().find('div.imageFrame').find('ul').css({
-          left: '0'
-        });
-      } else {
-        var current_left=$(this).parent().find('div.imageFrame').find('ul').css('left');
-        $(this).parent().find('div.imageFrame').find('ul').css({
-          left: (parseInt(current_left) - 50) + 'px'
-        });
-      }
-    });
-    $hobbyRightButton.click(function(event){
-      var sliderLength = $(this).parent().find('div.imageFrame').find('ul').find('li').length;
-      index--;
-      if (index < 0) {
-        index = sliderLength-1;
-        $(this).parent().find('div.imageFrame').find('ul').css({
-          left: '-' + ((sliderLength-1)*50) + 'px'
-        });
-      } else {
-        var current_left=$(this).parent().find('div.imageFrame').find('ul').css('left');
-        $(this).parent().find('div.imageFrame').find('ul').css({
-          left: (parseInt(current_left) + 50) + 'px'
-        });
-      }
+    var $galleryDisplayButton = $('#hobbyGallery').find('.three.columns').find('.hobbyButton');
+    var $galleryContent = $('#hobbyGallery').find('.nine.columns').find('p, section');
+    $galleryDisplayButton.on('click', function(event){
+      var $this = $(this);
+      var show_what=$this.attr('data-type');
+      $('#hobbyGallery').find('.nine.columns').find('.active').fadeOut("slow", function(){
+        $('#hobbyGallery').find('.nine.columns').find('.active').removeClass('active');
+        $galleryContent.parent().find('section.'+show_what).slideDown(1000);
+        $galleryContent.parent().find('section.'+show_what).addClass('active');
+      });
     });
   }
 
   navScroll();
-  // galleryDisplay();
+  galleryDisplay();
   galleryScroll();
-  // hobbyDisplay();
-  hobbyScroll();
+  hobbyDisplay();
 
 });
